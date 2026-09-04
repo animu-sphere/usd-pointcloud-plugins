9af3dbe596cb70ca526f12a5624787fe432fbec9
snakamura <88454117+snkmcb@users.noreply.github.com>
2026-09-04 23:34:42 +0900

    refactor: share resolver asset byte source (#140)
    * refactor: share resolver asset byte source
    
    * style: make shared adapter includes explicit

--- plugins/pointcloud-copc/include/usdgeocopc/ArAssetRandomAccessSource.h
+++ plugins/pointcloud-copc/include/usdgeocopc/ArAssetRandomAccessSource.h
@@ -1,29 +1,9 @@
 #pragma once
 
-#include "usdgeo/RandomAccessSource.h"
-
-#include <pxr/usd/ar/asset.h>
-
-#include <memory>
-#include <string>
+#include "usdgeo/ArAssetRandomAccessSource.h"
 
 namespace usdgeocopc {
 
-class ArAssetRandomAccessSource final : public usdgeo::RandomAccessSource {
-public:
-    ArAssetRandomAccessSource(std::shared_ptr<pxr::ArAsset> asset,
-                              std::string sourceName);
-
-    bool GetSize(std::uint64_t& size,
-                 std::vector<usdgeo::Diagnostic>& diagnostics) const override;
-    bool Read(std::uint64_t offset,
-              std::size_t size,
-              std::vector<std::uint8_t>& bytes,
-              std::vector<usdgeo::Diagnostic>& diagnostics) override;
-
-private:
-    std::shared_ptr<pxr::ArAsset> asset_;
-    std::string sourceName_;
-};
+using ArAssetRandomAccessSource = usdgeo::ArAssetRandomAccessSource;
 
 } // namespace usdgeocopc
